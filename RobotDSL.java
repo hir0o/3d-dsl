7cb4a84a18960a5473321ab276ce251f1c979b3e
mkmk4423 <mk.9.q.kyu@gmail.com>
2020-12-30 10:52:22 +0900

    Add: パーサを作成

--- RobotDSL.java
+++ RobotDSL.java
@@ -1,5 +1,31 @@
-public class RobotDSL {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import java.util.List;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
 
+public class RobotDSL {
+  public static void main(String[] args) {
+    Path path = Paths.get("text.txt");// 読み込むファイルを指定
+    List<String> allLines = null;// ファイルの内容を行ごどに格納するリスト
+    String delim = " ¥t,.¥n"; // 単語の区切り記号
+    String allText = "";// 全行を連結した文字列（初期値は長さ0の文字列""）
+    try {
+      allLines = Files.readAllLines(path, StandardCharsets.UTF_8);// 全行を一括読み込み
+    } catch (IOException e) {
+    }
+
+    for (String s : allLines) // 行末の改行も付け加えて全行連結
+      allText += (s + "¥n");
+
+    Context ct = new Context(allText, delim);// Contextオブジェクトの作成
+    Prog prog = new Prog();// パーサオブジェクトを作る
+
+    prog.parse(ct);// sに構文解析を指示（パーサメソッドを実行させる）
+  }
 }
 
 // （1）Prog → Com_list [EOF]
@@ -9,56 +35,120 @@ public class RobotDSL {
 // | move Fnum Fnum Fnum
 // （5）Fnum → [+-]?[0-9]+(.[0-9]+)?
 
-//(1)Prog -> Com_list
+// (1)Prog -> Com_list
 class Prog {
   private Com_List clist;
 
   public void parse(Context ct) {
-    //パーサ
+    clist = new Com_List(); // いきなり作って
+    clist.parse(ct);// 構文解析を指示
   }
+
   void exe() {
-    //エグゼキュータ
+    // エグゼキュータ
   }
 }
 
-//(2)Com_list → [Rep_com Prim_com] Com_list?
+// (2)Com_list → [Rep_com Prim_com] Com_list?
 class Com_List {
   private Rep_com rcom;
   private Prim_com pcom;
   private Com_List clist;
 
   public void parse(Context ct) {
-  // パーサ
+    if (ct.match("walk") || ct.match("move")) { // pcomだった場合
+      pcom = new Prim_com();
+      pcom.parse(ct);
+    } else if (ct.match("repeat")) { // repeatだった場合
+      rcom = new Rep_com();
+      rcom.parse(ct);
+    } else { // それ以外だった場合
+      System.out.println("無効な構文です: " + ct.currentToken());
+      System.exit(0); // 終了
+    }
+    ct.toNext(); // トークンを一つ進める
+    if (ct.match("walk") || ct.match("move") || ct.match("repeat")) {
+      clist = new Com_List();
+      clist.parse(ct);
+    }
   }
+
   void exe() {
-  //エグゼキュータ
+    // エグゼキュータ
   }
 }
 
-//(3) Rep_com → repeat { Com_list } [0-9]+
+// (3) Rep_com → repeat { Com_list } [0-9]+
 class Rep_com {
-  int inum = 0; //繰り返し回数
+  int inum = 0; // 繰り返し回数
   private Com_List clist;
+
   public void parse(Context ct) {
-  //パーサ
+    // パーサ
   }
+
   void exe() {
-  //エグゼキュータ
+    // エグゼキュータ
   }
 }
 
-//(4) Prim_com → walk sw? Fnum Fnum Fnum | move Fnum Fnum Fnum
+// (4) Prim_com → walk sw? Fnum Fnum Fnum | move Fnum Fnum Fnum
 class Prim_com {
   String op;
-  double fnum1, fnum2, fnum3; //各命令の引数
+  Fnum fnum1, fnum2, fnum3; // 各命令の引数
+
   public void parse(Context ct) {
-  // パーサ
+    op = ct.currentToken(); // 命令の種類（"walk"，”move”）をop に記録
+    if (ct.match("walk")) {
+      ct.toNext();
+      if (ct.match("sw")) {
+        // TODO: swの処理を考える
+      } else if (ct.match("[+-]?[0-9]+(.[0-9]+)?")) {
+        // 数字を記録
+        fnum1 = new Fnum();
+        fnum1.parse(ct);
+        if (ct.match("[+-]?[0-9]+(.[0-9]+)?")) {
+          fnum2 = new Fnum();
+          fnum2.parse(ct);
+        } else {
+          System.out.println("無効な構文です: " + ct.currentToken());
+          System.exit(0); // 終了
+        }
+        if (ct.match("[+-]?[0-9]+(.[0-9]+)?")) {
+          fnum3 = new Fnum();
+          fnum3.parse(ct);
+        } else {
+          System.out.println("無効な構文です: " + ct.currentToken());
+          System.exit(0); // 終了
+        }
+      } else {
+        System.out.println("無効な構文です: " + ct.currentToken());
+        System.exit(0); // 終了
+      }
+    }
   }
+
   void exe() {
-  //エグゼキュータ
-  //op に記録されている命令の種類に応じて処理を行う
+    // エグゼキュータ
+    // op に記録されている命令の種類に応じて処理を行う
   }
 }
 
+// (5) Fnum → [+-]?[0-9]+(.[0-9]+)?
+class Fnum {
+  private double val = 0; // 数値を格納
 
-//(5) Fnum → [+-]?[0-9]+(.[0-9]+)?
+  public void parse(Context ct) {
+    if (ct.currentToken() != null && ct.currentToken().matches("[+-]?[0-9]+(.[0-9]+)?")) {
+      val = Double.parseDouble(ct.currentToken());
+      ct.toNext(); // 次のトークンに進む
+    } else {
+      System.out.println("Num: 数字ではありません: " + ct.currentToken());
+      System.exit(0); // 終了
+    }
+  }
+
+  public void exe() {
+    // エグゼキュータ
+  }
+}
